ad10ea56396f138368eec57ee0aad1ed62aaf4f3
Sebastian Triana martinez <sebastian.triana@utp.edu.co>
2024-04-15 00:33:19 -0500

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { json } from '../../server/core/db/sequelize';
 
 async function fetchData() {
   try {
     console.log('Enviando solicitud al servidor...');
-    const response = await fetch('http://localhost:8080/api');
+    const response = await fetch('http://localhost:8099/products');
     console.log('Respuesta recibida del servidor:', response);
     const data = await response.json();
     console.log('Datos recibidos:', data);
@@ -14,7 +15,7 @@ async function fetchData() {
       console.log('Datos válidos recibidos. Representando en la interfaz de usuario...');
       ReactDOM.createRoot(document.getElementById('root')).render(
         <React.StrictMode>
-          <h1>{data.message}</h1>
+          <h1>{JSON.stringify(data)}</h1>
         </React.StrictMode>
       );
     } else {
